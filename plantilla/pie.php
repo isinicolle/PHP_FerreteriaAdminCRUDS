59e5e5c0e8f551a3dd332759244e0a5d8a3f0fd0
Isis Zapata <isin_zapataf@unicah.edu>
2022-02-17 18:24:53 -0600

    .
    .

---
+++ plantilla/pie.php
@@ -0,0 +1,20 @@
+  <!--Fin pesta;as-->
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Aviso</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              Si cierra sesi&oacute;n podr&iacute;a perder los datos en los que estaba trabajando.¿Desea continuar?
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+              <button type="button" class="btn btn-primary" > <a style="text-decoration: none;color: #fff;" href="./login.html">Cerrar sesión </a></button>
+            </div>
+          </div>
+        </div>
+      </div>
+</body>
+</html>
