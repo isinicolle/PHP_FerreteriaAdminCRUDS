f66e454826f8828dd3535852333d7360c1fb7460
Isis Zapata <isin_zapataf@unicah.edu>
2022-02-17 18:23:11 -0600

    .
    .

---
+++ plantilla/encabezado.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Pagina de inicio</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="./css/index.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+  <body>
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="./index.html">
+                <img width="120" class="d-inline-block align-text-top" src="./img/LogoParrilla.png">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div class="navbar-nav">
+                <a class="nav-link active" aria-current="page" href="./index.html">Compras</a>
+                <a class="nav-link " href="./formularioproductos.html">Productos</a>
+                <a class="nav-link" href="./Menu.html">Proveedores</a>
+                <a class="nav-link " href="./Crud_Empleados.html">Empleados &Backslash; Usuarios</a>
+                <a class="nav-link" href="#">Clientes</a>
+                <a class="nav-link" href="./contactenosAdmin.html">Envios</a>
+                <a class="nav-link" href="./listatrabajos.html">Direcciones</a>
+                <form class="d-flex">
+                  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Cerrar sesi&oacute;n</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </nav>
+  </header>
