115ebc866cc5f5680b35c43c1229f5b8d4053f15
David Chavez <davidferchavez2001@gmail.com>
2022-02-22 15:46:45 -0600

    Crud_Empleados.php

--- plantilla/encabezado.php
+++ plantilla/encabezado.php
@@ -1,5 +1,5 @@
 
-    <header>
+<header>
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
           <div class="container-fluid">
             <a class="navbar-brand" href="./index.html">
