c9d74e608fcbcce203ef22a113701b5088d71a4a
AlexDavidSandoval <sandovalex2001.as@gmail.com>
2022-02-24 16:03:32 -0600

    link compras

--- vistas/compras.php
+++ vistas/compras.php
@@ -69,7 +69,7 @@
               <a class="nav-link" href="./envios.php">Envios / Empresas </a>
               <a class="nav-link active" href="./compras.php">Compras</a>
               <a class="nav-link " href="./Crud_Empleados.html">Empleados &Backslash; Usuarios</a>
-              <a class="nav-link " href="./contactenosAdmin.html">Ventas</a>
+              <a class="nav-link " href="./ventas.php">Ventas</a>
               <a class="nav-link" href="./direcciones.php">Direcciones</a>
               <form class="d-flex">
                 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Cerrar sesi&oacute;n</button>
