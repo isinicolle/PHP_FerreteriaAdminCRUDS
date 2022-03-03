9e590014eb0335a25762a8198359fc2c8ee2a9b3
Isis Zapata <isin_zapataf@unicah.edu>
2022-03-03 14:38:34 -0600

    Update modeloDirecciones.php
    select de direcciones

--- modelos/modeloDirecciones.php
+++ modelos/modeloDirecciones.php
@@ -13,13 +13,14 @@ class direcciones
     }
 
        //funcion de listar
-       public function get_veiculos()
+       public function get_direcciones()
        {
-           $sql = "SELECT        dbo.Clientes.id_cliente, dbo.Clientes.nom_cliente, dbo.Clientes.apellido_cliente, dbo.Clientes.RTN, dbo.Clientes.direccion_cliente, dbo.Ciudades.nombre_ciudad, dbo.Clientes.tel_cliente, dbo.Clientes.estado, 
-           dbo.Clientes.DNI_Cliente,  dbo.usuariosClientes.nombre_usuario ,dbo.usuariosClientes.correo_usuario, dbo.usuariosClientes.estado AS Estado_Usuario
-FROM            dbo.usuariosClientes INNER JOIN
-           dbo.Clientes ON dbo.usuariosClientes.id_cliente = dbo.Clientes.id_cliente INNER JOIN
-           dbo.Ciudades ON dbo.Clientes.id_ciudad = dbo.Ciudades.id_ciudad";
+           $sql = "SELECT        dbo.DireccionesEnvio.id_direccionEnvio, dbo.DireccionesEnvio.direccion, dbo.Ciudades.nombre_ciudad, dbo.usuariosClientes.nombre_usuario, dbo.DireccionesEnvio.direccion_opcional, dbo.Ciudades.codigoPostal, 
+           dbo.Departamentos.nombreDepartamento
+FROM            dbo.DireccionesEnvio INNER JOIN
+           dbo.usuariosClientes ON dbo.DireccionesEnvio.id_usuarioCliente = dbo.usuariosClientes.id_usuarioCliente INNER JOIN
+           dbo.Ciudades ON dbo.DireccionesEnvio.id_ciudad = dbo.Ciudades.id_ciudad INNER JOIN
+           dbo.Departamentos ON dbo.Ciudades.id_departamento = dbo.Departamentos.id_departamento";
            $resultado = $this->db->query($sql);
 
            while($row = $resultado->fetch_assoc())
