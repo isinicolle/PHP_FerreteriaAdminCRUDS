77dbdb1b3e91652f684be265b8ffdd9d7b003f88
Isis Zapata <isin_zapataf@unicah.edu>
2022-03-03 14:13:19 -0600

    Update modeloclientes.php
    avance de modeloc lientes

--- modelos/modeloclientes.php
+++ modelos/modeloclientes.php
@@ -2,6 +2,14 @@
 
 class clientes 
 {
+    private $db;
+    private $clientesU;
+
+    public function construct()
+    {
+       $this -> clientesU = array();
+       $this -> db = new PDO( "sqlsrv:server=andresmunicah.database.windows.net;database=ferreteria_MovilWeb", "adminUnicah", "rootUnicah1");
+    }
 
 }
 
