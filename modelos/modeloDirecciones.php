a21d00d74159da8c3c8bdb9e366c387c3eadaeef
Isis Zapata <isin_zapataf@unicah.edu>
2022-03-03 14:17:58 -0600

    direcciones modeloa vance
    .

--- modelos/modeloDirecciones.php
+++ modelos/modeloDirecciones.php
@@ -5,11 +5,25 @@ class direcciones
     private $db;
     private $direcciones;
 
+    //funcion constructora
     public function construct()
     {
        $this -> direcciones = array();
        $this -> db = new PDO( "sqlsrv:server=andresmunicah.database.windows.net;database=ferreteria_MovilWeb", "adminUnicah", "rootUnicah1");
     }
+
+       //funcion de listar
+       public function get_veiculos()
+       {
+           $sql = "";
+           $resultado = $this->db->query($sql);
+
+           while($row = $resultado->fetch_assoc())
+           {
+               $this->direcciones[] = $row;
+           }
+           return $this->direcciones;
+       }
 
 }
 ?>
--- modelos/modeloclientes.php
+++ modelos/modeloclientes.php
@@ -5,11 +5,14 @@ class clientes
     private $db;
     private $clientesU;
 
+    //funcion constructora
     public function construct()
     {
        $this -> clientesU = array();
        $this -> db = new PDO( "sqlsrv:server=andresmunicah.database.windows.net;database=ferreteria_MovilWeb", "adminUnicah", "rootUnicah1");
     }
+
+    //funcion de listar
 
 }
 
