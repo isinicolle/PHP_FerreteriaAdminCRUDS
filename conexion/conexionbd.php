f1c100c5de1b31c7801282cbb4864c6ea2785e01
Isis Zapata <isin_zapataf@unicah.edu>
2022-03-13 19:16:24 -0600

    Referencia de las plantillas
    Quitanto vergueyo de arriba

--- conexion/conexionbd.php
+++ conexion/conexionbd.php
@@ -7,7 +7,7 @@
             try{
                  $this->bd = new PDO( "sqlsrv:server=andresmunicah.database.windows.net;database=ferreteria_MovilWeb", "adminUnicah", "rootUnicah1");
 
-                    echo("se conecto");
+                  
                }
                  catch(PDOException $exp){
                       echo("Error<br> $exp");
